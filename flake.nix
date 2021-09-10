7ae7d6b14405f73a8ead6cdadc55cab0fc4e04b5
chivay <chivay@users.noreply.github.com>
2021-09-10 00:01:27 +0000

    Apply automatic changes

--- flake.nix
+++ flake.nix
@@ -13,13 +13,13 @@
         packages = flake-utils.lib.flattenTree {
           zig-nightly = llvmPackages.stdenv.mkDerivation rec {
             pname = "zig";
-            version = "23c25c5eaf959549e2f8e090a6c0eae7cfa6cb30";
+            version = "93c6e31cf174c61e1c2e7c0bf53d9a09b3d91734";
 
             src = pkgs.fetchFromGitHub {
               owner = "ziglang";
               repo = pname;
               rev = version;
-              hash = "sha256-oI/hsCMwlfDcTjGGkV4WZhJHDZh6qvyGO4pmvCxgH7U=";
+              hash = "sha256-N6kiRlFtA0qo40wecGI/ro7hBJNbMWrfg9v1kl/pWaA=";
             };
 
             nativeBuildInputs = [ pkgs.cmake llvmPackages.llvm.dev ];
