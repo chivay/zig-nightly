ff3d316dec2a51a089ecd2c8fe7d56c95c2d388a
Hubert Jasudowicz <hubert.jasudowicz@gmail.com>
2022-07-17 10:48:39 +0200

    Fix upgrade to LLVM 14

--- flake.nix
+++ flake.nix
@@ -1,13 +1,14 @@
 {
   description = "zig-nightly";
 
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        llvmPackages = pkgs.llvmPackages_13;
+        llvmPackages = pkgs.llvmPackages_14;
       in
       rec {
         packages = flake-utils.lib.flattenTree {
@@ -21,6 +22,11 @@
               rev = version;
               hash = "sha256-EiLxh93mOZoTci7B8kIa0eApqVLbfPjPHUordp6ITKs=";
             };
+
+            # https://github.com/ziglang/zig/issues/12069 workaround
+            cmakeFlags = "
+            -DZIG_STATIC_ZLIB=on
+            ";
 
             nativeBuildInputs = [ pkgs.cmake llvmPackages.llvm.dev ];
             buildInputs = [ pkgs.libxml2 pkgs.zlib ]
