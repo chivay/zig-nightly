3f1e45b51addfd8cdd2bd776e66132b07662e61a
chivay <chivay@users.noreply.github.com>
2022-09-20 00:03:56 +0000

    Apply automatic changes

--- flake.nix
+++ flake.nix
@@ -36,13 +36,13 @@
 
           zig-nightly = llvmPackages.stdenv.mkDerivation rec {
             pname = "zig";
-            version = "e42f83825f1473661700f89ffd3060013261d605";
+            version = "4521456f66122c6348ef6980ae1eecdb94f4f110";
 
             src = pkgs.fetchFromGitHub {
               owner = "ziglang";
               repo = pname;
               rev = version;
-              hash = "sha256-ib6SXWmky/OSFb9Fyv2hPXDOaw4+z1N2Dqi2XW9e7YY=";
+              hash = "sha256-fjNtSpDBIHO0AA9miRxdLTr/l4BnXmroNpOIl3c6JQw=";
             };
 
             # https://github.com/ziglang/zig/issues/12069 workaround
